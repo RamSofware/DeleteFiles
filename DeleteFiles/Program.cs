accce3dba8033a1c6efc43c29baffa45c80c2947
MEDINA\tempuser <rjuarez71@gmail.com>
2018-09-14 15:00:06 -0400

    First Commit

---
+++ DeleteFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            { 
+            }
+            int aggedDays = 0;
+            Int32.TryParse(args[0], out aggedDays);
+            string dirName = args[1].ToString();
+            //string[] files = Directory.GetFiles();
+            Console.WriteLine("Working directory = " + dirName);
+            Console.WriteLine("agged days = " + args[0]);
+            Console.ReadKey();
+
+        }
+    }
+}
